fb766000064e2f611b256d523d8b79fa1a312485
Astarina Maulida <astarina.maulida@gmail.com>
2023-04-27 21:35:04 -0400

    Add Sidebar component

---
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+export function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <img className="logo" src="./logo.png" alt="logo" />
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
 export default function Home() {
   return (
-    <div>test</div>
-  )
+    <div>
+      <Sidebar />
+
+    </div>
+  );
 }
