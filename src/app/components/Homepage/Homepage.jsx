68c910978ccc6b43a82116e2a82081891f4da6ff
Astarina Maulida <astarina.maulida@gmail.com>
2023-04-28 08:00:37 -0400

    Create Map component

--- src/app/components/Homepage/Homepage.jsx
+++ src/app/components/Homepage/Homepage.jsx
@@ -1,8 +1,12 @@
+import { Map } from "../Map/Map.jsx";
+
 export function Homepage() {
   return (
     <div className="Homepage">
       <div className="container">
-        <div className="content-wrapper"></div>
+        <div className="content-wrapper">
+          <Map />
+        </div>
       </div>
     </div>
   );
