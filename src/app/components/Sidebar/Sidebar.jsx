5ba1a1c86b5ed4d7f8582631f20fa18e3b061d63
Astarina Maulida <astarina.maulida@gmail.com>
2023-05-01 23:56:02 -0400

    Add menu on sidebar

--- src/app/components/Sidebar/Sidebar.jsx
+++ src/app/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+
 export function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-content">
         <img className="logo" src="./logo.png" alt="logo" />
+        <ul>
+          <li>
+            <Link href="/" legacyBehavior>
+              <a>Map &amp; Data</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/graph" legacyBehavior>
+              <a>Graph</a>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
