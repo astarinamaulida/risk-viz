fe6253a4ecbb2c60ee603f56544650127da18a69
Astarina Maulida <astarina.maulida@gmail.com>
2023-05-01 21:37:47 -0400

    Add graph router

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+"use client"
+
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Homepage } from "./components/Homepage/Homepage";
+import { useRouter } from "next/navigation"; // import next/navigation
 
 export default function Home() {
+  const router = useRouter(); // use useRouter from next/navigation
+
+  const handleClick = () => {
+    router.push("/graph");
+  };
+  
   return (
     <div>
+       <button onClick={handleClick}>click me</button>
       <Sidebar />
       <Homepage />
     </div>
