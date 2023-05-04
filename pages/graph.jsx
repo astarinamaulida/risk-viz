6e3acd79c4ebaaad54783ae0d488e3404b685de8
Astarina Maulida <astarina.maulida@gmail.com>
2023-05-04 13:46:53 -0400

    Take out random color

--- pages/graph.jsx
+++ pages/graph.jsx
@@ -75,17 +75,6 @@ export default function Graph() {
     setChartData({ labels, datasets });
   }, [data, location, asset, businessCategory]);
 
-  const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
-  console.log;
-
   return (
     <div>
       <Sidebar />
